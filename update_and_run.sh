6e59fec3f7bea77028ab0664de6864b5ff0682c0
e-maks <matantsev0@gmail.com>
2023-02-16 03:28:25 +0300

    Add updater script

---
+++ update_and_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Specify the name of the Systemd service
+SERVICE_NAME="tgbot"
+SERVICE_NAME2="server"
+
+# Specify the path to the Git repository
+REPO_PATH="/opt/p2p_telegram_bot"
+
+# Navigate to the Git repository
+cd "$REPO_PATH"
+
+# Start an infinite loop
+while true; do
+  # Fetch the latest changes from the remote repository
+  git fetch
+
+  # Check if there are any new changes
+  if ! git diff --quiet HEAD origin/main; then
+    # Pull the latest changes from the remote repository
+    git pull
+
+    # Restart the Systemd service
+    systemctl restart "$SERVICE_NAME"
+    systemctl restart "$SERVICE_NAME2"
+
+    # Print a success message
+    echo "Updated Git repository and restarted $SERVICE_NAME"
+  fi
+
+  # Wait for a few seconds before checking for new changes again
+  sleep 10
+done
