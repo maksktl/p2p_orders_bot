a69ab3109efb7ce9622529af8299986d54cf952d
e-maks <matantsev0@gmail.com>
2023-02-18 04:52:37 +0300

    Move filters from constructor

--- tgbot/handlers/base.py
+++ tgbot/handlers/base.py
@@ -2,10 +2,10 @@ from aiogram import Dispatcher
 
 
 class BaseHandler:
+    _general_filters = {"bot_access": True}
 
     def __init__(self, dp: Dispatcher):
         self.dp = dp
-        self._general_filters = {"bot_access": True}
         self.register_methods()
 
     def register_methods(self):
