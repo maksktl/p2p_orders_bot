4faf6643d1ce96de32ca822297c589363db88f60
e-maks <matantsev0@gmail.com>
2023-02-18 05:14:49 +0300

    Add commands to control users and admins

--- tgbot/handlers/admin_hander.py
+++ tgbot/handlers/admin_hander.py
@@ -2,18 +2,44 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 
 from tgbot.handlers.base import BaseHandler
+from tgbot.services.user_service import UserService
 
 
 class AdminHandler(BaseHandler):
     def __init__(self, dp: Dispatcher):
         self._general_filters = {"bot_access": True, 'is_admin': True}
         super().__init__(dp)
-        self._general_filters['is_admin'] = True
 
     @staticmethod
     async def admin_start(message: Message):
-        await message.reply("Hello, admin!")
+        await message.reply("Добрый день, уважаемый администратор.")
+
+    @staticmethod
+    async def grant_access(message: Message):
+        command, user_tg_id = message.text.split()
+        user_tg_id = int(user_tg_id)
+        bot_access = '/grant_access' == command
+        await UserService.get_instance().set_user_settings(user_tg_id, bot_access=bot_access)
+        await message.answer(
+            f'<a href="tg://user?id={user_tg_id}">Пользователь</a> {"получил" if bot_access else "потерял"} доступ к боту')
+
+    @staticmethod
+    async def promote_user(message: Message):
+        command, user_tg_id = message.text.split()
+        user_tg_id = int(user_tg_id)
+        admin = '/promote' == command
+        await UserService.get_instance().set_user_settings(user_tg_id, admin=admin)
+        await message.answer(
+            f'<a href="tg://user?id={user_tg_id}">Пользователь</a> {"получил" if admin else "потерял"} права администратора')
 
     def register_methods(self):
-        self.dp.register_message_handler(AdminHandler.admin_start, commands=["start"], state="*",
+        self.dp.register_message_handler(AdminHandler.admin_start, commands=['start'], state='*',
+                                         **self._general_filters)
+        self.dp.register_message_handler(AdminHandler.grant_access, commands=['grant_access'], state='*',
+                                         **self._general_filters)
+        self.dp.register_message_handler(AdminHandler.promote_user, commands=['promote'], state='*',
+                                         **self._general_filters)
+        self.dp.register_message_handler(AdminHandler.grant_access, commands=['revoke_access'], state='*',
+                                         **self._general_filters)
+        self.dp.register_message_handler(AdminHandler.promote_user, commands=['revoke_promote'], state='*',
                                          **self._general_filters)
