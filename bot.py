6e48e605062f28bc16b762f54fc252106470d9bd
e-maks <matantsev0@gmail.com>
2023-02-26 02:12:00 +0300

    Log to file

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def main():
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
-        # filename='tgbot.log'
+        filename='tgbot.log'
     )
     logger.info("Starting bot")
     config = load_config(".env")
